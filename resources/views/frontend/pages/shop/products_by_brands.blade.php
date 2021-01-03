45a08efe62c182c0fe0dfa5c9571446ba6449f05
Ashiqur Rahman <ashiq.starit@gmail.com>
2021-01-03 13:38:14 +0600

    change in product filter

--- resources/views/frontend/pages/shop/products_by_brands.blade.php
+++ resources/views/frontend/pages/shop/products_by_brands.blade.php
@@ -606,3 +606,52 @@
         </div>
     </div>
 @endsection
+@push('js')
+    <script>
+        function filterSlider() {
+            var nonLinearSlider = document.getElementById('nonlinear');
+            if (typeof nonLinearSlider != 'undefined' && nonLinearSlider != null) {
+                noUiSlider.create(nonLinearSlider, {
+                    connect: true,
+                    behaviour: 'tap',
+                    start: [0, 1000],
+                    range: {
+                        min: 0,
+                        '10%': 100,
+                        '20%': 200,
+                        '30%': 300,
+                        '40%': 400,
+                        '50%': 500,
+                        '60%': 600,
+                        '70%': 700,
+                        '80%': 800,
+                        '90%': 900,
+                        max: 1000,
+                    },
+                });
+                var nodes = [
+                    document.querySelector('.ps-slider__min'),
+                    document.querySelector('.ps-slider__max'),
+                ];
+                nonLinearSlider.noUiSlider.on('update', function(values, handle) {
+                    //console.log(values)
+                    nodes[handle].innerHTML = Math.round(values[handle]);
+                    var filter_price = Math.round(values[handle]);
+                    console.log(filter_price)
+                    $.ajax({
+                        type: 'GET', //THIS NEEDS TO BE GET
+                        url: '',
+                        dataType: 'json',
+                        success: function (data) {
+                            console.log(data);
+
+
+                        },error:function(){
+                            console.log(data);
+                        }
+                    });
+                });
+            }
+        }
+    </script>
+@endpush
