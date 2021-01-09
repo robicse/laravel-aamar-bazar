aef92cefb7f595e72a8aeedc967df2312e0cf20d
Sayka Islam <sayka.starit@gmail.com>
2021-01-09 14:49:18 +0600

    Slider module

--- resources/views/frontend/includes/sliders.blade.php
+++ resources/views/frontend/includes/sliders.blade.php
@@ -1,3 +1,11 @@
 <div class="ps-shop-banner jump" style="padding: 10px;">
-    <div class="ps-carousel--nav-inside owl-slider" data-owl-auto="true" data-owl-loop="true" data-owl-speed="5000" data-owl-gap="0" data-owl-nav="true" data-owl-dots="true" data-owl-item="1" data-owl-item-xs="1" data-owl-item-sm="1" data-owl-item-md="1" data-owl-item-lg="1" data-owl-duration="1000" data-owl-mousedrag="on"><a href="#"><img src="{{asset('frontend/img/slider/shop-default/1.jpg')}}" alt=""></a><a href="#"><img src="{{asset('frontend/img/slider/shop-default/2.jpg')}}" alt=""></a></div>
+    @php
+    $sliders = \App\Model\Slider::all();
+    @endphp
+    <div class="ps-carousel--nav-inside owl-slider" data-owl-auto="true" data-owl-loop="true" data-owl-speed="5000" data-owl-gap="0" data-owl-nav="true" data-owl-dots="true" data-owl-item="1" data-owl-item-xs="1" data-owl-item-sm="1" data-owl-item-md="1" data-owl-item-lg="1" data-owl-duration="1000" data-owl-mousedrag="on">
+        @foreach($sliders as $slider)
+        <a href="{{$slider->url}}"><img src="{{asset('uploads/sliders/'.$slider->image)}}" alt=""></a>
+{{--        <a href="#"><img src="{{asset('frontend/img/slider/shop-default/2.jpg')}}" alt=""></a>--}}
+        @endforeach
+    </div>
 </div>
