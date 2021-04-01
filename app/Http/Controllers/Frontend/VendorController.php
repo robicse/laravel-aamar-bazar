d05ea7ae77e39b9f07cc4188a726ed1d3e31e036
Sayka Islam <sayka.starit@gmail.com>
2021-04-01 10:12:16 +0600

    Best Selling Product

--- app/Http/Controllers/Frontend/VendorController.php
+++ app/Http/Controllers/Frontend/VendorController.php
@@ -243,7 +243,7 @@ class VendorController extends Controller
         $shop = Shop::where('slug',$slug)->first();
         $shopCategories = ShopCategory::where('shop_id',$shop->id)->latest()->get();
         $shopBrands = ShopBrand::where('shop_id',$shop->id)->latest()->get();
-        $products = Product::where('added_by','seller')->where('user_id',$shop->id)->where('published',1)->where('num_of_sale', '>',0)->latest()->paginate(24);
+        $products = Product::where('added_by','seller')->where('user_id',$shop->id)->where('published',1)->where('num_of_sale', '>',0)->orderBy('num_of_sale', 'DESC')->latest()->paginate(24);
         return view('frontend.pages.shop.best_selling_products',compact('shop','shopCategories','shopBrands','products'));
     }
     public function bestSellingSubCategory($name,$slug,$sub) {
@@ -253,7 +253,7 @@ class VendorController extends Controller
         $subcategory = Subcategory::where('slug',$sub)->first();
         $shopCat = ShopCategory::where('shop_id',$shop->id)->latest()->get();
         $shopBrand = ShopBrand::where('shop_id',$shop->id)->latest()->get();
-        $products = Product::where('category_id',$category->id)->where('subcategory_id',$subcategory->id)->where('user_id',$shop->user_id)->where('published',1)->where('num_of_sale', '>',0)->latest()->paginate(24);
+        $products = Product::where('category_id',$category->id)->where('subcategory_id',$subcategory->id)->where('user_id',$shop->user_id)->where('published',1)->where('num_of_sale', '>',0)->orderBy('num_of_sale', 'DESC')->latest()->paginate(24);
         return view('frontend.pages.shop.best_selling_by_subcategory',compact('shop','category','subcategory','shopBrand','shopCat','products'));
     }
     public function bestSellingFilter($data,$sellerId)
@@ -263,7 +263,7 @@ class VendorController extends Controller
         $data2 = explode(',',$data);
         $data_min = (int) $data2[0];
         $data_max = (int) $data2[1];
-        $result_data = Product::where('user_id',$sellerId)->where('unit_price', '>=', $data_min)->where('unit_price', '<=', $data_max)->where('published',1)->where('num_of_sale', '>',0)->latest()->take(24)->get();
+        $result_data = Product::where('user_id',$sellerId)->where('unit_price', '>=', $data_min)->where('unit_price', '<=', $data_max)->where('published',1)->where('num_of_sale', '>',0)->orderBy('num_of_sale', 'DESC')->latest()->take(24)->get();
 
         return $result_data;
     }
@@ -276,7 +276,7 @@ class VendorController extends Controller
         $data2 = explode(',',$data);
         $data_min = (int) $data2[0];
         $data_max = (int) $data2[1];
-        $result_data = Product::where('user_id',$shop->user_id)->where('subcategory_id',$subcategory->id)->where('unit_price', '>=', $data_min)->where('unit_price', '<=', $data_max)->where('published',1)->where('num_of_sale', '>',0)->latest()->take(24)->get();
+        $result_data = Product::where('user_id',$shop->user_id)->where('subcategory_id',$subcategory->id)->where('unit_price', '>=', $data_min)->where('unit_price', '<=', $data_max)->where('published',1)->where('num_of_sale', '>',0)->orderBy('num_of_sale', 'DESC')->latest()->take(24)->get();
 
         return $result_data;
     }
