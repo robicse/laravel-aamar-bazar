9e3169c1f7b014e95337836cc7a6b54c0c73e1e9
DESKTOP-R5T7OPV\SAYKA <sayka.starit@gmail.com>
2021-09-12 12:40:25 +0600

    footer and product list Api

--- app/Http/Resources/ProductsListCollection.php
+++ app/Http/Resources/ProductsListCollection.php
@@ -19,6 +19,7 @@ class ProductsListCollection extends ResourceCollection
                     'name' => $data->name,
                     'thumbnail_image' => $data->thumbnail_img,
                     'current_stock' =>(integer) $data->current_stock,
+                    'unit' => $data->unit,
                     'base_price' => (double) $data->unit_price,
                     'base_discounted_price' => (double) home_discounted_base_price($data->id),
                     'discount' => (double) $data->discount,
